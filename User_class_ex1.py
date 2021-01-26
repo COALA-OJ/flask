07464b43183a69a639070e0f3dd846308e67d816
highcaslte01 <castle.h0326@gmail.com>
2021-01-26 17:43:11 +0900

    User_class_ex1.py

---
+++ User_class_ex1.py
@@ -0,0 +1,9 @@
+class User_class:
+    def __init__(self, id, password, name, garde, solved_number, solved_list, submit_list):
+        self.id = id
+        self.password = password
+        self.name = name
+        self.grade = garde
+        self.solved_number = solved_number
+        self.solved_list = solved_list
+        self.submit_list = submit_list
